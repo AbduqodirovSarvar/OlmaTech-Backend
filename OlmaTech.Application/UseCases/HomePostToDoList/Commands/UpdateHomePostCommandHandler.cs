c736f6b88f58699f851d429c8551080970758dff
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2024-08-07 10:09:09 +0500

    fixed

--- OlmaTech.Application/UseCases/HomePostToDoList/Commands/UpdateHomePostCommandHandler.cs
+++ OlmaTech.Application/UseCases/HomePostToDoList/Commands/UpdateHomePostCommandHandler.cs
@@ -33,7 +33,7 @@ namespace OlmaTech.Application.UseCases.HomePostToDoList.Commands
             homePost.TitleRu = request.TitleRu ?? homePost.TitleRu;
             homePost.TitleUz = request.TitleUz ?? homePost.TitleUz;
             homePost.TitleUzRu = request.TitleUzRu ?? homePost.TitleUzRu;
-            homePost.SubitleEn = request.SubitleEn ?? homePost.SubitleEn;
+            homePost.SubitleEn = request.SubtitleEn ?? homePost.SubitleEn;
             homePost.SubtitleRu = request.SubtitleRu ?? homePost.SubtitleRu;
             homePost.SubtitleUz = request.SubtitleUz ?? homePost.SubtitleUz;
             homePost.SubtitleUzRu = request.SubtitleUzRu ?? homePost.SubtitleUzRu;
