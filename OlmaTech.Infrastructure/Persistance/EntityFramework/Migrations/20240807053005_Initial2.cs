ef64fe3577bf5e6b31335ef02164b812d20ca7ba
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2024-08-07 10:30:17 +0500

    migration updated

---
+++ OlmaTech.Infrastructure/Persistance/EntityFramework/Migrations/20240807053005_Initial2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OlmaTech.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SubitleEn",
+                table: "HomePosts",
+                newName: "SubtitleEn");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SubtitleEn",
+                table: "HomePosts",
+                newName: "SubitleEn");
+        }
+    }
+}
