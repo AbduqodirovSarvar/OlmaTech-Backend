44f80cb5a16b3c34e332be8f1d045886c651ce13
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2024-08-12 17:25:48 +0500

    add some

--- OlmaTech.Application/UseCases/CommonToDoList/Queries/GetCommonDataQueryHandler.cs
+++ OlmaTech.Application/UseCases/CommonToDoList/Queries/GetCommonDataQueryHandler.cs
@@ -21,28 +21,36 @@ namespace OlmaTech.Application.UseCases.CommonToDoList.Queries
 
         public async Task<CommonViewModel> Handle(GetCommonDataQuery request, CancellationToken cancellationToken)
         {
-            var aboutTask = _appDbContext.Abouts.AsNoTracking().OrderByDescending(x => x.CreatedAt).FirstOrDefaultAsync(cancellationToken);
-            var homesTask = _appDbContext.HomePosts.AsNoTracking().ToListAsync(cancellationToken);
-            var servicesTask = _appDbContext.Services.AsNoTracking().ToListAsync(cancellationToken);
-            var projectsTask = _appDbContext.Projects.AsNoTracking().ToListAsync(cancellationToken);
-            var teamsTask = _appDbContext.Teams.AsNoTracking().ToListAsync(cancellationToken);
-            var clientsTask = _appDbContext.Clients.AsNoTracking().ToListAsync(cancellationToken);
-            var blogsTask = _appDbContext.BlogPosts.AsNoTracking().ToListAsync(cancellationToken);
-            var contactsTask = _appDbContext.Contacts.AsNoTracking().ToListAsync(cancellationToken);
-
-            await Task.WhenAll(aboutTask, homesTask, servicesTask, projectsTask, teamsTask, clientsTask, blogsTask, contactsTask);
-
-            return new CommonViewModel()
+            try
             {
-                About = _mapper.Map<AboutViewModel>(await aboutTask),
-                Homes = _mapper.Map<List<HomePostViewModel>>(await homesTask),
-                Services = _mapper.Map<List<ServiceViewModel>>(await servicesTask),
-                Projects = _mapper.Map<List<ProjectViewModel>>(await projectsTask),
-                Teams = _mapper.Map<List<TeamViewModel>>(await teamsTask),
-                Clients = _mapper.Map<List<ClientViewModel>>(await clientsTask),
-                Blogs = _mapper.Map<List<BlogPostViewModel>>(await blogsTask),
-                Contacts = _mapper.Map<List<ContactViewModel>>(await contactsTask)
-            };
+                var aboutTask = _appDbContext.Abouts.AsNoTracking().OrderByDescending(x => x.CreatedAt).FirstOrDefaultAsync(cancellationToken);
+                var homesTask = _appDbContext.HomePosts.AsNoTracking().ToListAsync(cancellationToken);
+                var servicesTask = _appDbContext.Services.AsNoTracking().ToListAsync(cancellationToken);
+                var projectsTask = _appDbContext.Projects.AsNoTracking().ToListAsync(cancellationToken);
+                var teamsTask = _appDbContext.Teams.AsNoTracking().ToListAsync(cancellationToken);
+                var clientsTask = _appDbContext.Clients.AsNoTracking().ToListAsync(cancellationToken);
+                var blogsTask = _appDbContext.BlogPosts.AsNoTracking().ToListAsync(cancellationToken);
+                var contactsTask = _appDbContext.Contacts.AsNoTracking().ToListAsync(cancellationToken);
+
+                await Task.WhenAll(aboutTask, homesTask, servicesTask, projectsTask, teamsTask, clientsTask, blogsTask, contactsTask);
+
+                return new CommonViewModel()
+                {
+                    About = _mapper.Map<AboutViewModel>(await aboutTask),
+                    Homes = _mapper.Map<List<HomePostViewModel>>(await homesTask),
+                    Services = _mapper.Map<List<ServiceViewModel>>(await servicesTask),
+                    Projects = _mapper.Map<List<ProjectViewModel>>(await projectsTask),
+                    Teams = _mapper.Map<List<TeamViewModel>>(await teamsTask),
+                    Clients = _mapper.Map<List<ClientViewModel>>(await clientsTask),
+                    Blogs = _mapper.Map<List<BlogPostViewModel>>(await blogsTask),
+                    Contacts = _mapper.Map<List<ContactViewModel>>(await contactsTask)
+                };
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message); // Re-throw the exception after logging
+            }
         }
+
     }
 }
